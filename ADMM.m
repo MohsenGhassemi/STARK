588fe555034f80f37ffe6f296b59654ccb097d45
MohsenGhassemi <48700005+MohsenGhassemi@users.noreply.github.com>
2019-03-18 21:55:56 -0400

    Update ADMM.m

--- ADMM.m
+++ ADMM.m
@@ -1,4 +1,4 @@
-function [D]=ADMM(Y, X, D_init, Permutation_vectors,Dictionary_sizes,lambda, gamma, MaxIter,tol)
+function D=ADMM(Y, X, D_init, Permutation_vectors,Dictionary_sizes,lambda, gamma, MaxIter,tol)
 
 % Y: observation matrix
 % X: coefficient matrix
@@ -12,14 +12,11 @@ function [D]=ADMM(Y, X, D_init, Permutation_vectors,Dictionary_sizes,lambda, gam
 % gamma: augmented Lagrangian parameter
 % MaxIter: Maximum number of iterations
 
-% Only rearrang_D_Dpi and flatten/unflatten do not work for K>3.
-
-%% Algorithm
 
 m = Dictionary_sizes{1};
 p=  Dictionary_sizes{2};
 K=length(m);
-size_dpi = fliplr(m.*p);% [m(2)*p(2) m(1)*p(1)];
+size_dpi = fliplr(m.*p);% [m(3)*p(3) m(2)*p(2) m(1)*p(1)];
 
 
 % Dictionary Initialization
@@ -33,7 +30,6 @@ W=cell(1,K);
 
 for k=1:K
     A{k}= rand(size_dpi);
-    %W{k}= rand(size_dpi);
     W{k}=D_pi_init;
 end
 %
@@ -41,8 +37,7 @@ end
 
 %% % perform the ADMM algorithm
 
-PvT=Permutation_vectors(:,2);
-% =(X kron I) vec(Y)= vec(YX^T) 
+PvT=Permutation_vectors(:,2); 
 unvec_1=Y*X';
 unperm_vec_1=unvec_1(:);
 vec_1=unperm_vec_1(PvT);
@@ -52,30 +47,8 @@ X_Gram=X*X';
 X_tilde_Gram=sparse(kron(X_Gram,speye(prod(m))));
 matr = X_tilde_Gram(PvT,PvT) + gamma*K*speye(prod(m)*prod(p));
 
-%Method (2):
-% X_tilde = sparse(kron(X,speye(prod(m))));
-% PvT=Permutation_vectors(:,2);
-% unperm_vec_1=X_tilde*Y(:);
-% vec_1=unperm_vec_1(PvT);
-% X_tilde_Gram=X_tilde*X_tilde';
-% matr = X_tilde_Gram(PvT,PvT) + gamma*K*speye(prod(m)*prod(p));
-
-%Method (3):
-% X_tilde = sparse(kron(X,speye(prod(m))));
-% PvT=Permutation_vectors(:,2);
-% Permuted_X_tilde=X_tilde(PvT,:);
-% vec_1=Permuted_X_tilde*Y(:);
-% matr = Permuted_X_tilde*Permuted_X_tilde' + gamma*K*speye(prod(m)*prod(p));
-
-%Method (4):
-% X_tilde = sparse(kron(X,speye(prod(m))));
-% P=Permutation_matrix;
-% vec_1 = P'*X_tilde*Y(:);permuting a vector is much faster than a matrix.
-% matr = P'*(X_tilde*X_tilde')*P + gamma*K*eye(prod(m)*prod(p));
-
 
 for iter=1:MaxIter
-    %    iter_admm=iter
     sum_A_W=zeros(size_dpi);
     
     for k=1:K
@@ -95,17 +68,8 @@ for iter=1:MaxIter
     end
     
     
-    %    D = normcols(rearrange_Dpi_D(D_pi,[m 1],[p 1]));
-    %    Objective_value(iter)=norm(Y-D*X,'fro')^2/2+lambda* sum_trace_norm(D_pi);
-    %     if iter>1 && abs(Objective_value(iter)-Objective_value(iter-1))<tol
-    % %                 display('ADMM Converged')
-    %
-    %         break
-    %     end
-    
 end
 
 D = normcols(rearrange_Dpi_D(D_pi,m,p));
-%Objective_value=norm(Y-D*X,'fro')^2/2+lambda* sum_trace_norm(D_pi);
 
 
